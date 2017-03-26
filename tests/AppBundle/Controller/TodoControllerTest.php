a231791b89dc3eff975c5eaf8ae6a150f04cc2b9
eklemen <ejklemen@gmail.com>
2017-03-26 14:22:36 -0400

    tests for todo complete 100%

--- tests/AppBundle/Controller/TodoControllerTest.php
+++ tests/AppBundle/Controller/TodoControllerTest.php
@@ -20,20 +20,78 @@ class TodoControllerTest extends TestCase
         $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
     }
 
-    public function testReadAllActionWithNotNullResult()
+    public function testReadAllActionWithValidResult()
     {
-        $results = null;
-        $repoStub = $this->createMock(TodoRepository::class);
-        $repoStub->method("find")->willReturn($results);
-        $repoStub->method("findAll")->willReturn($results);
-        $registryStub = $this->createMock(Registry::class);
-        $registryStub->method("getRepository")->willReturn($repoStub);
-        $controller = new TodoController($registryStub);
-        $result = $controller->readAllAction();
+        $controller = $this->createReadableController(new Response());
+        $result = $controller->readAllAction(1);
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_OK);
+    }
+
+    public function testReadActionWithNullResult()
+    {
+        $controller = $this->createReadableController(null);
+        $result = $controller->readAction(1);
         $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
     }
 
-    private function createReadableController($results = null) {
+    public function testReadActionWithValidResult()
+    {
+        $controller = $this->createReadableController(new Response());
+        $result = $controller->readAction(1);
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_OK);
+    }
+
+    public function testCreateAction() 
+    {
+        $registryStub = $this->createMock(Registry::class);
+        $managerStub = $this->createMock(EntityManager::class);
+        $registryStub->method("getManager")->willReturn($managerStub);
+        $controller = new TodoController($registryStub);
+        $result = $controller->createAction(new Request());
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_CREATED);
+    }
+
+    public function testDropActionWithNullResult() 
+    {
+        $controller = $this->createDeleteableController(null);
+        $result = $controller->dropAction(1);
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
+    }
+
+    public function testDropActionWithValidResult() 
+    {
+        $controller = $this->createDeleteableController(new Response());
+        $result = $controller->dropAction(1);
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_NO_CONTENT);
+    }
+
+    public function testUpdateActionWithNullResult() {
+        $repoStub = $this->createMock(TodoRepository::class);
+        $repoStub->method("find")->willReturn(null);
+        $managerStub = $this->createMock(EntityManager::class);
+        $registryStub = $this->createMock(Registry::class);
+        $registryStub->method("getManager")->willReturn($managerStub);
+        $registryStub->method("getRepository")->willReturn($repoStub);
+        $controller = new TodoController($registryStub);
+        $result = $controller->updateAction(123, new Request());
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
+    }
+
+    public function testUpdateActionWithValidResult() {
+        $todoStub = $this->createMock(Todo::class);
+        $repoStub = $this->createMock(TodoRepository::class);
+        $repoStub->method("find")->willReturn($todoStub);
+        $managerStub = $this->createMock(EntityManager::class);
+        $registryStub = $this->createMock(Registry::class);
+        $registryStub->method("getManager")->willReturn($managerStub);
+        $registryStub->method("getRepository")->willReturn($repoStub);
+        $controller = new TodoController($registryStub);
+        $result = $controller->updateAction(123, new Request());
+        $this->assertEquals($result->getStatusCode(), Response::HTTP_OK);
+    }
+
+    private function createReadableController($results = null) 
+    {
         $repoStub = $this->createMock(TodoRepository::class);
         $repoStub->method("find")->willReturn($results);
         $repoStub->method("findAll")->willReturn($results);
@@ -42,88 +100,20 @@ class TodoControllerTest extends TestCase
         return new TodoController($registryStub);
     }
 
-    // public function testReadActionWithValidResult()
-    // {
-    //     $expected = ['one', 'two', 'three'];
-    //     $controller = $this->createReadableController(['one', 'two', 'three']);
-    //     $result = $controller->readAction(1);
-    //     $this->assertEquals($result, $expected);
-    // }
+    private function createDeleteableController($result = null) 
+    {
+        $repoStub = $this->createMock(TodoRepository::class);
+        $repoStub->method("find")->willReturn($result);
+        $managerStub = $this->createMock(EntityManager::class);
+        $managerStub->method("getRepository")->willReturn($repoStub);
+        $registryStub = $this->createMock(Registry::class);
+        $registryStub->method("getManager")->willReturn($managerStub);
 
-    // public function testReadAllActionWithNullResult()
-    // {
-    //     $controller = $this->createReadableController(null);
-    //     $result = $controller->readAllAction();
-    //     $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
-    // }
+        return new TodoController($registryStub);
+    }
 
-    // public function testReadAllActionWithValidResult()
-    // {
-    //     $expected = ['one', 'two', 'three'];
-    //     $controller = $this->createReadableController(['one', 'two', 'three']);
-    //     $result = $controller->readAllAction(1);
-    //     $this->assertEquals($result, $expected);
-    // }
-
-    // public function testDropActionWithNullResult() {
-    //     $controller = $this->createDeleteableController(null);
-    //     $result = $controller->dropAction(1);
-    //     $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
-    // }
-
-    // public function testDropActionWithValidResult() {
-    //     $controller = $this->createDeleteableController(["one", "two", "three"]);
-
-    //     $result = $controller->dropAction(1);
-    //     $this->assertEquals($result->getStatusCode(), Response::HTTP_NO_CONTENT);
-    // }
-
-    // private function createDeleteableController($result = null) {
-    //     $repoStub = $this->createMock(TodoRepository::class);
-    //     $repoStub->method("find")->willReturn($result);
-
-    //     $managerStub = $this->createMock(EntityManager::class);
-    //     $managerStub->method("getRepository")->willReturn($repoStub);
-
-    //     $registryStub = $this->createMock(Registry::class);
-    //     $registryStub->method("getManager")->willReturn($managerStub);
-
-    //     return new TodoController($registryStub);
-    // }
-
-    // private function createReadableController($results = null) {
-    //     $repoStub = $this->createMock(TodoRepository::class);
-    //     $repoStub->method("find")->willReturn($results);
-    //     $repoStub->method("findAll")->willReturn($results);
-    //     $registryStub = $this->createMock(Registry::class);
-    //     $registryStub->method("getRepository")->willReturn($repoStub);
-    //     return new TodoController($registryStub);
-    // }
-
-    // public function testCreateAction() {
-    //     $registryStub = $this->createMock(Registry::class);
-    //     $managerStub = $this->createMock(EntityManager::class);
-    //     $registryStub->method("getManager")->willReturn($managerStub);
-    //     $controller = new TodoController($registryStub);
-    //     $result = $controller->createAction(new Request());
-    //     $this->assertEquals($result->getStatusCode(), Response::HTTP_CREATED);
-    // }
-
-    // public function testUpdateActionWithNullResult() {
-    //     $repoStub = $this->createMock(TodoRepository::class);
-    //     $repoStub->method("find")->willReturn(null);
-
-    //     $managerStub = $this->createMock(EntityManager::class);
-
-    //     $registryStub = $this->createMock(Registry::class);
-
-    //     $registryStub->method("getManager")->willReturn($managerStub);
-    //     $registryStub->method("getRepository")->willReturn($repoStub);
-
-    //     $controller = new TodoController($registryStub);
-    //     $result = $controller->updateAction(123, new Request());
-    //     $this->assertEquals($result->getStatusCode(), Response::HTTP_NOT_FOUND);
-    // }
+    
+    
 
     // public function testUpdateActionWithValidResult() {
     //     $todoStub = $this->createMock(Todo::class);
